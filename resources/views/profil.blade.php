2f608dd802ff9ab32082c3cd463b46a3052f37c9
Ilhamf380 <milhamfauzan@students.amikom.ac.id>
2026-04-10 23:38:40 +0700

    update laravel project

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -16,7 +16,7 @@
 <div class="bg-white p-8 rounded-xl shadow-lg text-center">
     <h1 class="text-2xl font-bold mb-4">Profil Praktikan</h1>
     <p class="mb-4">Nama: Muhammad Ilham</p>
-    <p class="mb-6">NIM: ISI NIM KAMU</p>
+    <p class="mb-6">NIM: 24.12.3342</p>
 
     <a href="/" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Home</a>
 </div>
