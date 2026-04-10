b0955ec5a1137e1f3af1fd40d97d48e867cf67cc
Ilhamf380 <milhamfauzan@students.amikom.ac.id>
2026-04-10 23:45:16 +0700

    Update profil.blade.php

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Profil</title>
+    <title>Katalog</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <div class="bg-blue-600 text-white p-4 flex gap-4">
@@ -11,13 +11,28 @@
     <a href="/bantuan" class="hover:underline">Bantuan</a>
     <a href="/contact" class="hover:underline">Kontak</a>
 </div>
-<body class="bg-slate-100 flex items-center justify-center h-screen">
+<body class="bg-gray-100 p-10">
 
-<div class="bg-white p-8 rounded-xl shadow-lg text-center">
-    <h1 class="text-2xl font-bold mb-4">Profil Praktikan</h1>
-    <p class="mb-4">Nama: Muhammad Ilham Fauzan</p>
-    <p class="mb-6">NIM: 24.12.3342</p>
+<h1 class="text-3xl font-bold mb-6 text-center">Katalog Event</h1>
 
+<div class="grid grid-cols-3 gap-6">
+    <div class="bg-white p-4 rounded-lg shadow hover:shadow-xl">
+        <h2 class="font-bold">Event Musik</h2>
+        <p>Konser seru di Amikom</p>
+    </div>
+
+    <div class="bg-white p-4 rounded-lg shadow hover:shadow-xl">
+        <h2 class="font-bold">Seminar IT</h2>
+        <p>Belajar teknologi terbaru</p>
+    </div>
+
+    <div class="bg-white p-4 rounded-lg shadow hover:shadow-xl">
+        <h2 class="font-bold">Workshop</h2>
+        <p>Praktek langsung skill</p>
+    </div>
+</div>
+
+<div class="text-center mt-6">
     <a href="/" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Home</a>
 </div>
 
