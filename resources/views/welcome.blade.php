4d1507d4c6e71708978f839825a7709e4717f21a
Ilhamf380 <milhamfauzan@students.amikom.ac.id>
2026-04-10 23:41:05 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
-<h1>Home Event Amikom</h1>
-
-<a href="/profil">Profil</a>
-<a href="/katalog">Katalog</a>
-<a href="/bantuan">Bantuan</a>
-<a href="/contact">Kontak</a>
+<script src="https://cdn.tailwindcss.com"></script>
+<div class="bg-blue-600 text-white p-4 flex gap-4">
+    <a href="/" class="hover:underline">Home</a>
+    <a href="/profil" class="hover:underline">Profil</a>
+    <a href="/katalog" class="hover:underline">Katalog</a>
+    <a href="/bantuan" class="hover:underline">Bantuan</a>
+    <a href="/contact" class="hover:underline">Kontak</a>
+</div>
